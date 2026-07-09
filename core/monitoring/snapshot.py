220afaeb32354432eebd625569cb39e19d612294
book2012 <ttlfig@gmail.com>
2026-07-09 06:53:46 +0000

    Add monitoring snapshot

---
+++ core/monitoring/snapshot.py
@@ -0,0 +1,37 @@
+from core.power.manager import PowerManager
+from core.session.session import SessionManager
+from core.task.registry import TaskRegistry
+from core.worker.factory import WorkerFactory
+
+
+class MonitoringSnapshot:
+    def __init__(
+        self,
+        registry: TaskRegistry | None = None,
+        worker_factory: WorkerFactory | None = None,
+    ):
+        self.registry = registry or TaskRegistry()
+        self.worker_factory = worker_factory or WorkerFactory()
+        self.session = SessionManager(self.registry)
+        self.power = PowerManager(
+            registry=self.registry,
+            worker_factory=self.worker_factory,
+        )
+
+    def collect(self, workers: list[str]):
+        data = {}
+
+        for worker_name in workers:
+            worker = self.worker_factory.create(worker_name)
+
+            data[worker_name] = {
+                "worker": worker.status(),
+                "session": {
+                    "state": self.session.state(worker_name).value,
+                    "can_shutdown": self.session.can_shutdown(worker_name),
+                    "running_tasks": len(self.registry.running(worker_name)),
+                },
+                "power": self.power.shutdown_status(worker_name),
+            }
+
+        return data
